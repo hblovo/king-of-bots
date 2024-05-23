f4e4eec503682844ca550be289bfa4efa3169b46
hblovo <694186378@qq.com>
2024-05-23 11:15:17 +0800

    添加查询用户所有bot的功能

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -52,15 +52,29 @@ export default {
         //         console.log(resp);
         //     }
         // })
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/update/",
+        //     type: "POST",
+        //     data: {
+        //         bot_id:"12",
+        //         title: "Bot的标题3",
+        //         description : "Bot of hblovo",
+        //         content: "Hello world",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
         $.ajax({
-            url: "http://127.0.0.1:3000/user/bot/update/",
-            type: "POST",
-            data: {
-                bot_id:"12",
-                title: "Bot的标题3",
-                description : "Bot of hblovo",
-                content: "Hello world",
-            },
+            url: "http://127.0.0.1:3000/user/bot/getlist/",
+            type: "get",
+            
             headers: {
                 Authorization: "Bearer " + store.state.user.token,
             },
